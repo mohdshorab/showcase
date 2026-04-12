ed633c9da808348f6878b8acce900ddde988a0bf
Mohd Shorab <mohod.shorab@KELLGGNLPTP2008.local>
2026-04-13 01:03:43 +0530

    config: add tailwind css v4

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import type React from "react";
 
 const App: React.FC = () => {
-  return <h1>Hello World!</h1>;
+  return <h1 className="text-2xl text-red-500" >Hello World!</h1>;
 };
 export default App;
